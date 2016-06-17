f839861854f2a04f2235f22d980ed3226982d1d9
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-06-16 23:24:13 -0700

    add solution for 92

--- python practice/LinkedList/reverseListii.py
+++ python practice/LinkedList/reverseListii.py
@@ -22,34 +22,20 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        if head is not None or head.next is not None:
+        if m == n:
             return head
         dummy = mylist.ListNode(0)
         dummy.next = head
-        head1 = dummy
+        before_reverve = dummy
         for i in range(m - 1):
-            head1 = head1.next
-        p = head1.next
+            before_reverve = before_reverve.next
+        reverse_head = before_reverve.next
         for i in range(n - m):
-            tmp = head1.next
-            head1.next = p.next
-            p.next = p.next.next
-            head1.next.next = tmp
+            tmp = before_reverve.next
+            before_reverve.next = reverse_head.next
+            reverse_head.next = reverse_head.next.next
+            before_reverve.next.next = tmp
         return dummy.next
-        # if m == n:
-        #     return head
-        # dummy = mylist.ListNode(0)
-        # dummy.next = head
-        # before_reverve = dummy
-        # for i in range(m - 1):
-        #     before_reverve = before_reverve.next
-        # reverse_head = before_reverve.next
-        # for i in range(n - m):
-        #     tmp = before_reverve.next
-        #     before_reverve.next = reverse_head.next
-        #     before_reverve.next.next = tmp
-        #     reverse_head.next = reverse_head.next.next
-        # return dummy.next
 
 
 nodes = mylist.createList([1, 2, 3, 4, 5, 6])
