63daad4b99ce3b1271d507a6b61f0ac9ed73fb91
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-06-21 22:29:04 -0700

    solution for linked list cycle

---
+++ python practice/LinkedList/linkedlistCycle.py
@@ -0,0 +1,22 @@
+# 141. Linked List Cycle
+# Given a linked list, determine if it has a cycle in it.
+
+# Follow up:
+# Can you solve it without using extra space?
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None or head.next is None:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
