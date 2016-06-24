285c70745a21f26f6774d7f66c74b03b8a1a879f
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-06-23 23:26:28 -0700

    more solutions: insertion sort, remove

---
+++ python practice/LinkedList/insertionSortList.py
@@ -0,0 +1,27 @@
+# 147. Insertion Sort List
+# Sort a linked list using insertion sort.
+
+
+class Solution(object):
+    def insertionSortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        curr = head
+        while curr.next:
+            if curr.next.val < curr.val:
+                pre = dummy
+                while pre.next.val < curr.next.val:
+                    pre = pre.next
+                tmp = curr.next
+                curr.next = tmp.next
+                tmp.next = pre.next
+                pre.next = tmp
+            else:
+                curr = curr.next
+        return dummy.next
---
+++ python practice/LinkedList/removeEle.py
@@ -0,0 +1,31 @@
+# 203. Remove Linked List Elements
+# Remove all elements from a linked list of integers that have value val.
+
+# Example
+# Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+# Return: 1 --> 2 --> 3 --> 4 --> 5
+
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if head is None:
+            return head
+        while head.val == val:
+            head = head.next
+            if head is None:
+                return head
+        slow = head
+        fast = slow.next
+        while fast:
+            if fast.val == val:
+                slow.next = fast.next
+                fast = fast.next
+            else:
+                slow = slow.next
+                fast = fast.next
+        return head
