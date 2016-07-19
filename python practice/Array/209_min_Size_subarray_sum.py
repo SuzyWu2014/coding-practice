f3726d498fd6c51c04d6e8761335bff9a453854e
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-07-18 23:18:05 -0700

    add problem @209

---
+++ python practice/Array/209_min_Size_subarray_sum.py
@@ -0,0 +1,16 @@
+# 209. Minimum Size Subarray Sum
+# Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+# For example, given the array [2,3,1,2,4,3] and s = 7,
+# the subarray [4,3] has the minimal length under the problem constraint.
+# More practice:
+# If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+
+
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
