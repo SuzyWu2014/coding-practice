7d98868e9d8c053b66c10bbf553d116ea82751eb
shujinwu <shujinwu@oregonstate.edu>
2016-08-21 22:14:50 -0700

    update #279

--- python practice/BFS/279_perfect_squares.py
+++ python practice/BFS/279_perfect_squares.py
@@ -3,6 +3,8 @@
 
 # For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
 import collections
+import sys
+import math
 
 
 class Solution(object):
@@ -24,4 +26,49 @@ class Solution(object):
                 y += 1
         return dp[n]
 
-print Solution().numSquares(12)
+    def __init__(self):
+        self.dp = [0]
+
+    def numSquares2(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        for i in range(len(self.dp), n + 1):
+            result = sys.maxsize
+            k = int(math.sqrt(i))
+            if k * k == i:
+                self.dp.append(1)
+                continue
+            for j in range(1, int(math.sqrt(i)) + 1):
+                result = min( self.dp[i - j * j] + 1, result)
+                if result == 2:
+                    break
+            self.dp.append(result)
+        return self.dp[n]
+
+    def numSquares3(self, n):
+        if n < 2:
+            return n
+        sqr_list = []
+        i = 1
+        while i * i <= n:
+            sqr_list.append(i * i)
+            i += 1
+        to_check = {n}
+        cnt = 0
+        while to_check:
+            cnt += 1
+            next_to_check = set()
+            for num in to_check:
+                for item in sqr_list:
+                    if num == item:
+                        return cnt
+                    if num < item:
+                        break
+                    next_to_check.add(num - item)
+            to_check = next_to_check
+        return cnt
+
+
+print Solution().numSquares3(4)
