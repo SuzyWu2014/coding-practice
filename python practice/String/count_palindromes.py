0fd6b43d4717fee18a83b0227bf4b532a6bcfa2c
shujinwu <shujinwu@oregonstate.edu>
2016-09-10 17:23:15 -0400

    update count_palindromes

--- python practice/String/count_palindromes.py
+++ python practice/String/count_palindromes.py
@@ -1,23 +1,22 @@
-def countPalindromes(string):
+def countPalindromes(S):
     count = 0
-    for i in range(len(string)):
+    for i in range(len(S)):
         count += 1
         k, t = i - 1, i + 1
-        if k >= 0 and string[k] == string[i]:
+        if k >= 0 and S[k] == S[i]:
             # hllh
             count += 1
             k -= 1
-            count += countPalinInPosition(k, t, string)
+            count += countPalinInPosition(k, t, S)
         k, t = i - 1, i + 1
-        count += countPalinInPosition(k, t, string)
+        count += countPalinInPosition(k, t, S)
         # print count
     return count
 
-def countPalinInPosition(k, t, string):
+def countPalinInPosition(k, t, S):
     count = 0
-    print k, t
-    while k >= 0 and t < len(string):
-        if string[k] == string[t]:
+    while k >= 0 and t < len(S):
+        if S[k] == S[t]:
             count += 1
             k -= 1
             t += 1
