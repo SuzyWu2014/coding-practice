fcf48dc43e0b91887c36cd66d305a082f1bb7715
shujinwu <shujinwu@oregonstate.edu>
2016-10-30 12:10:32 -0700

    add knapsack_bounded bottom up

--- cs-519-master Algorithm/HW6/knapsack_bounded.py
+++ cs-519-master Algorithm/HW6/knapsack_bounded.py
@@ -7,13 +7,31 @@ def best(weight, items):
     What's the best value for a bag of W?
     (0, -1, -1): max_val, index of curr item, # of curr item
     """
-    d = [(0, -1, -1) for i in xrange(weight + 1)]
+    d = [[0] * (len(items) + 1) for i in xrange(weight + 1)]
+    for i in xrange(weight + 1):
+        d[i][0] = (0, 0, 0)
+    for i in xrange(len(items) + 1):
+        d[0][i] = (0, i, 0)
+
     for w in xrange(1, weight + 1):
         for i, (wi, vi, ci) in enumerate(items):
-            vals = [(d[w - wi * k][0] + vi * k, i, k)
+            vals = [(d[w - wi * k][i][0] + vi * k, i + 1, k )
                         for k in xrange(1, ci + 1) if w >= wi * k]
-            d[w] = max(max(d), max(vals, key=lambda x: (x[0], -x[1])) if len(vals) > 0 else (0, -1, -1))
-    print(d[weight])
+            tmp = max(vals, key=lambda x: (x[0], -x[1])) if len(vals) > 0 else (0, i + 1, 0)
+            d[w][i + 1] = max(d[w][i], tmp, key=lambda x: (x[0], -x[1]))
+
+    return (d[weight][len(items)][0], back_trace(weight, items, d))
+
+
+def back_trace(weight, items, d):
+    rst = [0] * len(items)
+    curr = len(items)
+    while weight >= 0 and curr > 0:
+        _, curr, cnt = d[weight][curr]
+        rst[curr - 1] = cnt
+        weight -= items[curr - 1][0] * cnt
+        curr -= 1
+    return rst
 
 
 
