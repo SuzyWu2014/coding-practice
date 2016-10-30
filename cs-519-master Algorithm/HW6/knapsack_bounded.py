61b3d902cd1d5d35934445e59c7b8d17edb88fc7
shujinwu <shujinwu@oregonstate.edu>
2016-10-30 00:06:02 -0700

    update knapsack_unbounded bottom-up

---
+++ cs-519-master Algorithm/HW6/knapsack_bounded.py
@@ -0,0 +1,9 @@
+def best(weight, items):
+    """
+    Bounded Knapsack
+
+    You have n items, each with weight w_i and value v_i, and has c_i copies.
+    **All numbers are positive integers.**
+    What's the best value for a bag of W?
+    """
+    pass
--- cs-519-master Algorithm/HW6/knapsack_unbounded.py
+++ cs-519-master Algorithm/HW6/knapsack_unbounded.py
@@ -6,26 +6,27 @@ def best(weight, items):
     **All numbers are positive integers.**
     What's the best value for a bag of W?
     """
-    d = [0 for i in xrange(weight + 1)]
-    back = [0 for i in xrange(weight + 1)]
+    d = [(0, -1) for i in xrange(weight + 1)]
 
     for w in xrange(1, weight + 1):
-        vals = [(d[w - item[0]] + item[1], i) for i, item in enumerate(items) if w >= item[0]]
-        d[w] = max(vals)[0] if len(vals) > 0 else 0
-        for val, i in vals:
-            if val == d[w]:
-                back[w] = i
-                break
-    count = back_trace(weight, items, back)
-    return (d[weight], count)
+        vals = [(d[w - item[0]][0] + item[1], i) for i, item in enumerate(items) if w >= item[0]]
+        d[w] = max(vals, key=lambda x: (x[0], -x[1])) if len(vals) > 0 else (0, -1)
 
-def back_trace(weight, items, back):
+    count = back_trace(weight, items, d)
+    return (d[weight][0], count)
+
+
+def back_trace(weight, items, d):
     count = [0 for i in xrange(len(items))]
     while weight > 0:
-        item = back[weight]
+        item = d[weight][1]
         count[item] += 1
         weight -= items[item][0]
     return count
+
+
+# def best2(weight, items):
+# pass
 
 
 print best(3, [(2, 4), (3, 5)])
