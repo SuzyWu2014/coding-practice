3c6c6a66b96f557c41fa1ffdb0b8bcada21a3797
shujinwu <shujinwu@oregonstate.edu>
2016-10-29 23:46:24 -0700

    add knapsack_unbounded

---
+++ cs-519-master Algorithm/HW6/knapsack_unbounded.py
@@ -0,0 +1,36 @@
+def best(weight, items):
+    """
+    Unbounded Knapsack
+
+    You have n items, each with weight w_i and value v_i, and has infinite copies.
+    **All numbers are positive integers.**
+    What's the best value for a bag of W?
+    """
+    d = [0 for i in xrange(weight + 1)]
+    back = [0 for i in xrange(weight + 1)]
+
+    for w in xrange(1, weight + 1):
+        vals = [(d[w - item[0]] + item[1], i) for i, item in enumerate(items) if w >= item[0]]
+        d[w] = max(vals)[0] if len(vals) > 0 else 0
+        for val, i in vals:
+            if val == d[w]:
+                back[w] = i
+                break
+    count = back_trace(weight, items, back)
+    return (d[weight], count)
+
+def back_trace(weight, items, back):
+    count = [0 for i in xrange(len(items))]
+    while weight > 0:
+        item = back[weight]
+        count[item] += 1
+        weight -= items[item][0]
+    return count
+
+
+print best(3, [(2, 4), (3, 5)])
+print best(3, [(1, 5), (1, 5)])
+print best(3, [(1, 2), (1, 5)])
+print best(3, [(1, 2), (2, 5)])
+print best(58, [(5, 9), (9, 18), (6, 12)])
+print best(92, [(8, 9), (9, 10), (10, 12), (5, 6)])
