aa36bdbcbd4ceb02a7e8bc590a63774445877c64
shujinwu <shujinwu@oregonstate.edu>
2016-10-30 21:10:32 -0700

    update lis

--- cs-519-master Algorithm/HW6/lis.py
+++ cs-519-master Algorithm/HW6/lis.py
@@ -3,22 +3,23 @@ def lis(string):
     Longest (Strictly) Increasing Subsequence
        - input/output are lower-case strings:
     """
+    if len(string) == 0:
+        return ""
     d = dict()
-    d[0] = (1, -1, True)
+    d[0] = (1, -1)
     for i in xrange(1, len(string)):
-        vals = [(d[j][0] + 1, j, True) for j in xrange(i) if string[j] < string[i]]
-        d[i] = max(vals) if len(vals) > 0 else (1, -1, True)
+        vals = [(d[j][0] + 1, j) for j in xrange(i) if string[j] < string[i]]
+        d[i] = max(vals) if len(vals) > 0 else (1, -1)
 
     return back_trace(string, d)
 
 
 def back_trace(string, d):
     rst = []
-    curr = len(string) - 1
+    curr = max(d, key=d.get)
     while curr > -1:
-        _, pre, if_curr = d[curr]
-        if if_curr:
-            rst.append(string[curr])
+        _, pre = d[curr]
+        rst.append(string[curr])
         curr = pre
 
     return "".join(reversed(rst))
@@ -30,3 +31,4 @@ if __name__ == '__main__':
     print(lis(""))
     print(lis("aaaa"))
     print(lis("aabbaacfgs"))
+    print(lis("bacpsa"))
