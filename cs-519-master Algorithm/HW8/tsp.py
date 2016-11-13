40f3e10698c48a90dca587418dbb82b4c12ac316
shujinwu <shujinwu@oregonstate.edu>
2016-11-12 19:57:00 -0800

    update tsp

--- cs-519-master Algorithm/HW8/tsp.py
+++ cs-519-master Algorithm/HW8/tsp.py
@@ -7,23 +7,33 @@ def tsp(cities):
     """
     @cities: (index, x, y)
     """
+    if cities is None or len(cities) <= 1:
+        return 0
     size = len(cities)
     route = collections.namedtuple("route", "distance pre_city")
-    cache = collections.defaultdict(lambda: route(float("inf"), -1))
+    cache = collections.defaultdict(lambda: route(distance=float("inf"),
+                                                  pre_city=-1))
     # From city 0 to city i
     for i, sub in enumerate(city_sets(size, 1)):
-        cache[(i + 1, sub)] = route(distance(cities, 0, i + 1), 0)
+        cache[(i + 1, sub)] = route(distance=distance(cities, 0, i + 1),
+                                    pre_city=0)
 
     # From city 0 to city k via a set of cities(i..m..k)
     for cnt in xrange(2, size):
         for subset in city_sets(size, cnt):
             for city in subset:
                 pre_set = tuple(i for i in subset if i != city)
-                tmp = min([route(cache[(m, pre_set)].distance + distance(cities, m, city), pre_city=m) for m in pre_set])
+                tmp = min([
+                    route(distance=cache[(m, pre_set)].distance + distance(cities, m, city),
+                          pre_city=m)
+                    for m in pre_set])
                 cache[(city, subset)] = min(cache[(city, subset)], tmp)
 
     # Back to city 0
-    cache[(0, tuple(xrange(0, size)))] = min([route(cache[(i, tuple(xrange(1, size)))].distance + distance(cities, i, 0), pre_city=i) for i in xrange(1, size)])
+    cache[(0, tuple(xrange(0, size)))] = min([
+        route(distance=cache[(i, tuple(xrange(1, size)))].distance + distance(cities, i, 0),
+              pre_city=i)
+        for i in xrange(1, size)])
 
     return cache[(0, tuple(xrange(0, size)))].distance, back_trace(size, cache)
 
@@ -59,6 +69,9 @@ def distance(cities, src, dest):
 #     return res
 
 if __name__ == '__main__':
+    print(tsp([]))
+    print(tsp([(0, 0, 0)]))
+    print(tsp([(0, 0, 0), (1, 1, 1)]))
     print(tsp([(0, 0, 0), (1, 1, 1), (2, 1, 0), (3, 0, 1)]))
     print(tsp([(0, 1, 0), (1, 0, 1), (2, 0, 0), (3, 1, 1)]))
     print(tsp([(0, 0, 0), (1, 1, 1), (2, 1, 0), (3, 0, 1), (4, 0.5, 1.5)]))
