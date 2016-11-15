e78dce96aba176519cb1dc6f1c56746354b34521
shujinwu <shujinwu@oregonstate.edu>
2016-11-14 18:04:52 -0800

    update mask info

--- python practice/info_mask.py
+++ python practice/info_mask.py
@@ -1,16 +1,38 @@
-def info_mask():
-    typ, info = raw_input().strip().split(":")
-    if typ == 'E':
-        name, domain = info.strip().split("@")
-        name = name[0] + "*****" + name[len(name) - 1]
-        print typ + ":" + name + "@" + domain
-    else:
+# -*- coding: utf-8 -*-
+
+import re
+
+
+def info_mask(s):
+    typ, info = s.split(":")
+    info = info.strip()
+    print info
+    if typ == 'E' and re.match(r'^[^\.].*', info) and re.match(r'.*[^\.]@.*', info) and re.match(r'.*\.\..*', info) is None and re.match(r'[\w\.!#$%&’*+-/=?^_`{|}~]*@.*\..*', info):
+            name, domain = info.strip().split("@")
+            name = name[0] + "*****" + name[len(name) - 1]
+            print typ + ":" + name + "@" + domain
+    elif typ == 'P' and re.match(r"\+?([\d]{0,3})[\s\(\)-]([\d]{3})[\s\(\)-]([\d]{3})[\s\(\)-]([\d]{4})", info):
         nums = info[len(info) - 4:]
         cnt_nums = sum([1 for i in xrange(len(info) - 4) if '0' <= info[i] <= '9'])
-        pre = "*" * (cnt_nums % 3)
-        pre = "***-***-" if len(pre) == 0 else pre + "-***-***-"
-        if info[0] == '+':
-            pre = "+" + pre
-        print typ + ":" + pre + nums
+        if  6 <= cnt_nums <= 9:
+            pre = "*" * (cnt_nums % 3)
+            pre = "***-***-" if len(pre) == 0 else pre + "-***-***-"
+            if info[0] == '+':
+                pre = "+" + pre
+            print "P:%s%s" % (pre, nums)
 
-info_mask()
+info_mask("E:s..uzy@gmail.com")
+info_mask("E:suzy.@gmail.com")
+info_mask("E:.suzy@gmail.com")
+info_mask("E:!suzy@gmail.com")
+# info_mask("P:+1-917-123-2345")
+# info_mask("P:+1-917 123-2345")
+
+
+r'^[^\.].*'
+r'.*[^\.]$@.'
+r'\.\2'
+r'[\w\.!#$%&’*+-/=?^_`{|}~]*@.*\..*'
+
+
+"\+?[\d\s\(\)-]*$"
