8a16d03ed1c3cdf453f5e8bbb3b1ee8e9d8be540
shujinwu <shujinwu@oregonstate.edu>
2016-11-27 11:15:33 -0800

    add solution for nbest

--- cs-519-master Algorithm/Chanllenge/rna.py
+++ cs-519-master Algorithm/Chanllenge/rna.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, namedtuple
+import heapq
 
 
 def best(sequence):
@@ -90,27 +91,40 @@ def kbest(sequence, k):
        by tagging each nucleotide as (, ., or ). Each matching pair of () must be
        AU, GC, or GU (or their mirror symmetries: UA, GC, UG).
        We also assume pairs can _not_ cross each other.
-       The following are valid structures for ACAGU:
+
+        k-best structures: output the 1-best, 2nd-best, ... kth-best structures.
     """
-    trace = namedtuple("track", "count is_curr_matching pre")
-    dp = defaultdict(set)
-    # lambda: trace(count=0, is_curr_matching=False, pre=float("inf"))
+    dp = defaultdict(list)
+    for i in xrange(len(sequence)):
+        dp[i, i].append((0, ["."]))
+        dp[i, i - 1].append((0, ["."]))
+
     for size in xrange(2, len(sequence) + 1):
         for i in xrange(len(sequence) - size + 1):
             if isPair(sequence[i], sequence[i + size - 1]):
-                dp[i, i + size - 1].add(trace(count=dp[i + 1, i + size - 2][0].count + 1,
-                                              is_curr_matching=True,
-                                              pre=float("inf")))
-            tmp = max([trace(count=dp[i, i + j].count + dp[i + j + 1, i + size - 1].count,
-                             is_curr_matching=False,
-                             pre=i + j)
-                                for j in xrange(size - 1)],
-                        key=lambda x:(x[0], x[2])) # x[2] take larger (i + j)
+                new = [(cnt + 1, ["("] + pre + [")"]) for cnt, pre in dp[i + 1, i + size - 2]]
+                dp[i, i + size - 1].extend(new)
 
-            # key=lambda x:(x[0], -x[1]):  -x[1] means to take is_curr_matching=True over is_curr_matching=False
-            dp[i, i + size - 1] = max(dp[i, i + size - 1], tmp)
+            dp[i, i + size - 1].extend([(cnt, pre + ["."])
+                        for cnt, pre in dp[i, i + size - 2]])
 
-    return back_trace(dp, len(sequence))
+            for t in xrange(i + 1, i + size - 1):
+                if isPair(sequence[i + size - 1], sequence[t]):
+                    for lcnt, left in dp[i, t - 1]:
+                        for rcnt, right in dp[t + 1, i + size - 2]:
+                            dp[i, i + size - 1].append((lcnt + rcnt + 1, left + ["("] + right +[")"]))
+
+    res = sorted(dp[i, i + size - 1], reverse=True)
+    return ["".join(pairs) for cnt, pairs in res[:k]]
+    # heap = []
+    # for i, (cnt, pairs) in enumerate(dp[0, len(sequence) - 1]):
+    #     if i < k:
+    #         heapq.heappush(heap, (cnt, "".join(pairs)))
+    #     elif heap[0][0] < cnt:
+    #         heapq.heapreplace(heap, (cnt, "".join(pairs)))
+
+    # return [pairs for cnt, pairs in heap]
+
 
 if __name__ == '__main__':
     # print best("UUCAGGA")
@@ -134,38 +148,39 @@ if __name__ == '__main__':
 
     print best("ACAGU")
     print total("ACAGU")
+    print kbest("ACAGU", 1)
 
-    print best("AC")
-    print total("AC")
+    # print best("AC")
+    # print total("AC")
 
-    print best("GUAC")
-    print total("GUAC")
+    # print best("GUAC")
+    # print total("GUAC")
 
-    print best("GCACG")
-    print total("GCACG")
+    # print best("GCACG")
+    # print total("GCACG")
 
-    print best("CCGG")
-    print total("CCGG")
+    # print best("CCGG")
+    # print total("CCGG")
 
-    print best("CCCGGG")
-    print total("CCCGGG")
+    # print best("CCCGGG")
+    # print total("CCCGGG")
 
-    print best("UUCAGGA")
-    print total("UUCAGGA")
+    # print best("UUCAGGA")
+    # print total("UUCAGGA")
 
-    print best("AUAACCUA")
-    print total("AUAACCUA")
+    # print best("AUAACCUA")
+    # print total("AUAACCUA")
 
-    # print total("GUUAGAGUCU")
+    # # print total("GUUAGAGUCU")
 
-    print best("UUGGACUUG")
-    print total("UUGGACUUG")
+    # print best("UUGGACUUG")
+    # print total("UUGGACUUG")
 
-    print best("UUUGGCACUA")
-    print total("UUUGGCACUA")
+    # print best("UUUGGCACUA")
+    # print total("UUUGGCACUA")
 
-    print best("GAUGCCGUGUAGUCCAAAGACUUC")
-    print total("GAUGCCGUGUAGUCCAAAGACUUC")
+    # print best("GAUGCCGUGUAGUCCAAAGACUUC")
+    # print total("GAUGCCGUGUAGUCCAAAGACUUC")
 
-    print best("AGGCAUCAAACCCUGCAUGGGAGCG")
-    print total("AGGCAUCAAACCCUGCAUGGGAGCG")
+    # print best("AGGCAUCAAACCCUGCAUGGGAGCG")
+    # print total("AGGCAUCAAACCCUGCAUGGGAGCG")
